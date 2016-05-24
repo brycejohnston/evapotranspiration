65ab1c380b5811896619fca624ff916c84d9a19c
Bryce Johnston <johnstonbrc@gmail.com>
2016-05-24 12:43:02 -0500

    Finish porting thornthwaite, but not tested

--- lib/evapotranspiration/thornthwaite.rb
+++ lib/evapotranspiration/thornthwaite.rb
@@ -11,8 +11,8 @@ module Evapotranspiration
   # climate. Geographical Review, 38, 55-94.
   module Thornthwaite
 
-    MONTHDAYS = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-    LEAP_MONTHDAYS = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    MONTHDAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    LEAP_MONTHDAYS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     # Estimate monthly potential evapotranspiration (PET) using the
     # Thornthwaite (1948) method.
@@ -57,25 +57,27 @@ module Evapotranspiration
         month_days = LEAP_MONTHDAYS
       end
 
-      ### *** Everything below still needs ported into Ruby ***
-
       # Negative temperatures should be set to zero
-      adj_monthly_t = [t * (t >= 0) for t in monthly_t]
+      adj_monthly_t = []
+      monthly_t.each do |t|
+        adj_monthly_t << (t >= 0 ? t : 0)
+      end
 
       # Calculate the heat index (heat_index)
       heat_index = 0.0
-      for tai in adj_monthly_t:
-          if tai / 5.0 > 0.0:
-              heat_index += (tai / 5.0) ** 1.514
+      adj_monthly_t.each do |tai|
+        if (tai / 5.0) > 0.0
+          heat_index += (tai / 5.0) ** 1.514
+        end
+      end
 
       a = (6.75e-07 * heat_index ** 3) - (7.71e-05 * heat_index ** 2) + (1.792e-02 * heat_index) + 0.49239
 
       pet = []
-      for ta, l, n in zip(adj_monthly_t, monthly_mean_dlh, month_days):
-          # Multiply by 10 to convert cm/month --> mm/month
-          pet.append(
-              1.6 * (l / 12.0) * (n / 30.0) * ((10.0 * ta / heat_index) ** a) * 10.0)
-
+      adj_monthly_t.zip(monthly_mean_dlh, month_days).each do |ta, l, n|
+        # Multiply by 10 to convert cm/month --> mm/month
+        pet << 1.6 * (l / 12.0) * (n / 30.0) * ((10.0 * ta / heat_index) ** a) * 10.0
+      end
       return pet
     end
 
@@ -100,17 +102,17 @@ module Evapotranspiration
       monthly_mean_dlh = []
       doy = 1 # Day of the year
 
-      ### *** Everything below still needs ported into Ruby ***
-
-      for mdays in month_days:
-         dlh = 0.0  # Cumulative daylight hours for the month
-         for daynum in range(1, mdays + 1):
-             sd = fao.sol_dec(doy)
-             sha = fao.sunset_hour_angle(latitude, sd)
-             dlh += fao.daylight_hours(sha)
-             doy += 1
-         # Calc mean daylight hours of the month
-         monthly_mean_dlh.append(dlh / mdays)
+      month_days.each do |mdays|
+        dlh = 0.0  # Cumulative daylight hours for the month
+        [1..(mdays + 1)].each do |daynum|
+          sd = FAO.sol_dec(doy)
+          sha = FAO.sunset_hour_angle(latitude, sd)
+          dlh += FAO.daylight_hours(sha)
+          doy += 1
+        end
+        # Calc mean daylight hours of the month
+        monthly_mean_dlh << (dlh / mdays)
+      end
 
       return monthly_mean_dlh
     end
